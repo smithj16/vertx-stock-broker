b75f011278dfa341fbdaa97d731f0bb231b72d41
smithj16 <jacobsmithevans@gmail.com>
2023-01-28 22:30:51 -0600

    added logback file

--- src/main/java/com/jacob/vertx_stock_broker/MainVerticle.java
+++ src/main/java/com/jacob/vertx_stock_broker/MainVerticle.java
@@ -2,9 +2,26 @@ package com.jacob.vertx_stock_broker;
 
 import io.vertx.core.AbstractVerticle;
 import io.vertx.core.Promise;
+import io.vertx.core.Vertx;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class MainVerticle extends AbstractVerticle {
 
+  private static final Logger log = LoggerFactory.getLogger(MainVerticle.class);
+  public static void main(String[] args) {
+    Vertx vertx = Vertx.vertx();
+    vertx.exceptionHandler(error -> {
+      log.error("Unhandled: {}", error);
+    });
+    vertx.deployVerticle(new MainVerticle(), ar -> {
+      if(ar.failed()) {
+        log.error("Failed to deploy verticle. Error: {}", ar.cause());
+        return;
+      }
+      log.info("Verticle Deployed {}", MainVerticle.class);
+    });
+  }
   @Override
   public void start(Promise<Void> startPromise) throws Exception {
     vertx.createHttpServer().requestHandler(req -> {
